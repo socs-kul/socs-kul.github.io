d6424e18953cebb32c0c714adbefa7fd48292d93
ignacevn <92166686+ignacevn@users.noreply.github.com>
2023-08-02 11:07:01 +0200

    Delete _exercises/4-dynamic-memory directory

--- _exercises/4-dynamic-memory/ex1.c
+++
@@ -1,8 +0,0 @@
-void sum(int *in1, int *in2, int *out, int n) {
-  for (int i = 0; i < n; i++) {
-    *out = *in1 + *in2;
-    out++;
-    in1++;
-    in2++;
-  }
-}
--- _exercises/4-dynamic-memory/ex4.asm
+++
@@ -1,5 +0,0 @@
-allocate_stack:
-    mv t0, a0
-    mv a0, sp
-    sub sp, sp, t0
-    ret
